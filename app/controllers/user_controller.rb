e3ab9e13eb588c4b1ee18e1bb6c9fcf83208ced3
Darrin Holst <darrinholst@gmail.com>
2013-03-23 14:20:49 -0500

    404

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
   def show
     @user = User.find_by_username(params[:user])
-    redirect_to root_url unless @user
+    render :file => "#{Rails.root}/public/404.html", :status => :not_found, :layout => false unless @user
   end
 end
